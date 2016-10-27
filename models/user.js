4f186aa6617900f6c3bca6d75673d247df021df3
Zachary Fong <fong.zachary@gmail.com>
2016-10-27 16:32:33 -0700

    require bcrypt-nodejs

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 var Exam = require('./exam');
 
 var UserSchema = new Schema({
