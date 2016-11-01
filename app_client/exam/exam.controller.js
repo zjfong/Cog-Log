fb2ea33bcf0dd756795ab46d4365297f68ba7860
Zachary Fong <fong.zachary@gmail.com>
2016-11-01 12:07:04 -0700

    adds angular charts

--- app_client/exam/exam.controller.js
+++ app_client/exam/exam.controller.js
@@ -45,6 +45,39 @@ function examController($location, $http, authentication) {
     });
   };
 
+  // var totalList = vm.examsList.map(function totalScore(exam){
+  //   return exam.totalScore;
+  // })
+
+  vm.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  vm.series = ['Series A', 'Series B'];
+  vm.data = [
+    // [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  vm.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  vm.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  vm.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+
 
   // vm.cleanDate = function(){
   //   vm.examsList.map(function examDate(exam){
--- app_client/main.js
+++ app_client/main.js
@@ -1,5 +1,5 @@
 angular
-    .module('health', ['ngRoute', 'ngAnimate', 'ngSanitize','ui.bootstrap', 'ngMaterial', 'ui.bootstrap'])
+    .module('health', ['ngRoute', 'ngAnimate', 'ngSanitize','ui.bootstrap', 'ngMaterial', 'ui.bootstrap', 'chart.js'])
     .config(config)
     .run(run);
 
