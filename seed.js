b45ce846ebdbc260d7239afe3988e2d6a9d7d8f4
Zachary Fong <fong.zachary@gmail.com>
2016-11-02 15:29:54 -0700

    attempt fix seed

--- seed.js
+++ seed.js
@@ -1,6 +1,5 @@
 // var User = require('./app_api/models/user');
-var db = require('./app_api/models/db');
-console.log(db)
+require('./app_api/models/db');
 
 db.User.find({}, function(err, users){
   console.log('found all users')
