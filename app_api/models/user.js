86013686cdb73e27bf36a87e9ff5339ce5352d34
Zachary Fong <fong.zachary@gmail.com>
2016-11-03 12:20:09 -0700

    changes secret to env var

--- app_api/models/user.js
+++ app_api/models/user.js
@@ -36,7 +36,7 @@ userSchema.methods.generateJwt = function() {
     email: this.email,
     name: this.name,
     exp: parseInt(expiry.getTime() / 1000),
-  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+  }, process.env.JWT_SECRET); // DO NOT KEEP YOUR SECRET IN THE CODE!
 };
 
 var User = mongoose.model('User', userSchema);
